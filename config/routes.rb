e08c9b309865cef0e94daee6708a6246823c8acd
Corrina <corrina.calanoc@berkeley.edu>
2015-03-12 21:35:50 -0700

    I'M DONE

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get "users", to: "users#index"
-  get "users/new", to: "users#new"
-  get "users/:id", to: "users#show", as: :user
-  post "users", to: "users#create"
+  get "dogs", to: "users#index"
+  get "dogs/new", to: "users#new"
+  get "dogs/:id", to: "users#show", as: :dog
+  post "dogs", to: "users#create"
 end
